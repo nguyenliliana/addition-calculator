401081d9336c163aa278d12e488d2fd9d41a7776
Liliana <liliananguyen03@gmail.com>
2022-08-22 22:23:38 -0700

    change to calculate mode of number list

--- processing.py
+++ processing.py
@@ -1,2 +1,11 @@
-def do_calculation(number1, number2):
-    return number1 + number2
+import statistics
+
+
+def calculate_mode(number_list):
+    try:
+        return "The mode of the numbers is {}".format(statistics.mode(number_list))
+    except statistics.StatisticsError as exc:
+        return "Error calculating mode: {}".format(exc)
+
+# def do_calculation(number1, number2):
+#    return number1 + number2
