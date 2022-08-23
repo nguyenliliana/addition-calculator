258ae1ab2eec02e4b7de60da09f418be978efdc1
Liliana <liliananguyen03@gmail.com>
2022-08-22 21:54:44 -0700

    html elements

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from processing import do_calculation
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route('/', methods=["GET", "POST"])
+def adder_page():
+    return '''
+     <html>
+      <body>
+       <p> Enter your numbers: </p>
+       <form method="post" action".">
+        <p><input name="number1" /></p>
+        <p><input name="number2" /></p>
+        <p><input type="submit" value="Do calculation" /></p>
+       </form>
+      </body>
+     </html>
+    '''
 
 
 if __name__ == '__main__':
