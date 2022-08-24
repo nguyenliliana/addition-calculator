2aef57532c24862700e4ec6130b6b1441a1f3a8e
Liliana <liliananguyen03@gmail.com>
2022-08-23 21:20:49 -0700

    finished home, addition, and mode page

--- views.py
+++ views.py
@@ -1,15 +1,25 @@
 from flask import Blueprint, request
-from processing import do_calculation
+from processing import calculate_sum, calculate_mode
 views = Blueprint(__name__, "views")
 
 
 @views.route("/")
 def home():
-    return "home page"
+    return '''
+    <html>
+     <body>
+      <div style="display:flex; flex-direction: horizontal; align-items: center;" >
+       <a href ="/"><img src='https://iconarchive.com/download/i83704/custom-icon-design/mono-general-3/home.ico' style="width:auto; height: 1.5em; " ></a>
+       <h1> &nbsp; Home </h1>
+      </div>
+      <p style="font-size: 20px;" ><a href ="/addition">Addition Calculator </a>
+      <p style="font-size: 20px;" ><a href ="/mode">Mode Calculator </a>
+    </html>
+    '''
 
 
 @views.route('/addition', methods=["GET", "POST"])
-def adder_page():
+def addition_page():
     errors = ""
     if request.method == "POST":
         number1 = None
@@ -25,7 +35,7 @@ def adder_page():
             errors += "<p>{!r} is not a number.</p>\n".format(
                 request.form["number2"])
         if number1 is not None and number2 is not None:
-            result = do_calculation(number1, number2)
+            result = calculate_sum(number1, number2)
             return '''
           <html>
            <body>
@@ -37,6 +47,10 @@ def adder_page():
     return '''
         <html>
          <body>
+          <div style="display:flex; flex-direction: horizontal; align-items: center;" >
+           <a href ="/"><img src='https://iconarchive.com/download/i83704/custom-icon-design/mono-general-3/home.ico' style="width:auto; height: 1.5em; " ></a>
+           <h1> &nbsp; Addition Calculator </h1>
+          </div>
           {errors}
           <p> Enter your numbers: </p>
           <form method="post" actions=".">
@@ -49,6 +63,51 @@ def adder_page():
     '''.format(errors=errors)
 
 
-@views.route("/mode")
-def mode():
-    return "mode page"
+inputs = []
+
+
+@views.route("/mode", methods=["GET", "POST"])
+def mode_page():
+    errors = ""
+    numbers_so_far = ""
+    if request.method == "POST":
+        try:
+            inputs.append(float(request.form["number"]))
+        except:
+            errors += "<p>{!r} is not a number.</p>\n".format(
+                request.form["number"])
+        if request.form["action"] == "Calculate number":
+            result = calculate_mode(inputs)
+            inputs.clear()
+            return '''
+             <html>
+              <body>
+               <p>{result}</p>
+               <p><a href="/mode">Click here to calculate again</a>
+              </body>
+             </html>
+            '''.format(result=result)
+    if len(inputs) == 0:
+        numbers_so_far = ""
+    else:
+        numbers_so_far = "<p>Numbers so far: </p>"
+        for number in inputs:
+            numbers_so_far += "<p>{}</p>".format(number)
+    return '''
+        <html>
+            <body>
+            <div style="display:flex; flex-direction: horizontal; align-items: center;" >
+             <a href ="/"><img src='https://iconarchive.com/download/i83704/custom-icon-design/mono-general-3/home.ico' style="width:auto; height: 1.5em; " ></a>
+             <h1> &nbsp; Mode Calculator </h1>
+            </div>
+                {numbers_so_far}
+                {errors}
+                <p>Enter your number:</p>
+                <form method="post" actions=".">
+                    <p><input name="number" /></p>
+                    <p><input type="submit" name="action" value="Add another" /></p>
+                    <p><input type="submit" name="action" value="Calculate number" /></p>
+                </form>
+            </body>
+        </html>
+    '''.format(numbers_so_far=numbers_so_far, errors=errors)
