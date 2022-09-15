336c780218ee43d0e718b8d07b01e2de72d7c47e
nguyenliliana <liliananguyen@ucsb.edu>
2022-09-14 22:16:06 -0700

    Multiplication and division

--- processing.py
+++ processing.py
@@ -10,3 +10,17 @@ def calculate_mode(number_list):
 
 def calculate_sum(number1, number2):
     return number1 + number2
+
+
+def calculate_difference(number1, number2):
+    return number1-number2
+
+
+def calculate_product(number1, number2):
+    return number1*number2
+
+
+def calculate_quotient(number1, number2):
+    if number2 == 0:
+        return "Undefined"
+    return number1/number2
