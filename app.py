68b356730faf3419081e5abdb371e1f9eda1c531
nguyenliliana <liliananguyen@ucsb.edu>
2022-10-08 00:00:33 -0700

    Finish.

--- app.py
+++ app.py
@@ -4,48 +4,6 @@ from views import views
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
-#app.config["DEBUG"] = True
-
-
-# @app.route('/', methods=["GET", "POST"])
-# def adder_page():
-#     errors = ""
-#     if request.method == "POST":
-#         number1 = None
-#         number2 = None
-#         try:
-#             number1 = float(request.form["number1"])
-#         except:
-#             errors += "<p>{!r} is not a number.</p>\n".format(
-#                 request.form["number1"])
-#         try:
-#             number2 = float(request.form["number2"])
-#         except:
-#             errors += "<p>{!r} is not a number.</p>\n".format(
-#                 request.form["number2"])
-#         if number1 is not None and number2 is not None:
-#             result = do_calculation(number1, number2)
-#             return '''
-#           <html>
-#            <body>
-#             <p> The result is {result} </p>
-#             <p><a href ="/">Click here to calculate again </a>
-#            <body>
-#           </html>
-#          '''.format(result=result)
-#     return '''
-#         <html>
-#          <body>
-#           {errors}
-#           <p> Enter your numbers: </p>
-#           <form method="post" actions=".">
-#            <p><input name="number1" /></p>
-#            <p><input name="number2" /></p>
-#            <p><input type="submit" value="Do calculation" /></p>
-#           </form>
-#          </body>
-#         </html>
-#     '''.format(errors=errors)
 
 
 if __name__ == '__main__':
